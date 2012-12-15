8842cb69a398972016e1c6e3c93d0b579fbc5f50
Martin Gonzalez <tinchogonz@gmail.com>
2012-12-14 22:05:51 -0300

    Commit inicial

---
+++ src/com/sopranos/pongclone/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sopranos.pongclone;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.Scene;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+public class MainActivity extends SimpleBaseGameActivity implements  IOnSceneTouchListener {
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
